e3b32feaba4fc8ddaf8992cf0183ec0ac01fb58c
Sujay Chand <sujaygchand@gmail.com>
2021-12-31 23:11:52 +1300

    Enabled cors and Newtonsoft

--- EmployeesTest_PostGres/Startup.cs
+++ EmployeesTest_PostGres/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,18 @@ namespace EmployeesTest_PostGres
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			//Enable CORS
+			services.AddCors(c =>
+			{
+				c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+			});
+
+
+			//JSON Serializer
+			services.AddControllersWithViews().AddNewtonsoftJson(options =>
+			options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+				.AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+				= new DefaultContractResolver());
 
 			services.AddControllers();
 			// Loading PosgreSQL
